4866d92adc07f724281bf21e61e09d2b57b9a988
wcontayon <williamcontayon@outlook.com>
2020-11-03 20:18:34 +0100

    Update ps1 file to run Docker MSSQL-Linux

--- database_test_setup.ps1
+++ database_test_setup.ps1
@@ -1,4 +1,5 @@
 # Setup database test on docker
+# oy2ntsnzu5gyqfysk5yaxim4hazaxjfelcgvlfc2dukrki
 # We use Sql Server for linux
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=!#123Pwd@!" -p 1402:1433 --name fluentDb -h fluentDb -d mcr.microsoft.com/mssql/server:2019-latest
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=!#123Pwd@!" -p 1402:1433 --name fluentDb -h fluentDb -d hadem/fluentdb-mssql-linux:fluentDbMsSqlLinux
